1dd10435890bc60036dfc5adc9b0d8d0a08ee7af
aritoka <atknoop@gmail.com>
2021-11-16 16:52:37 +0100

    move netsquid rlated functions to example

--- examples/wobbly_function/src/qcgpilot_netsquid.py
+++ examples/wobbly_function/src/qcgpilot_netsquid.py
@@ -8,7 +8,57 @@ from qcg.pilotjob.api.job import Jobs
 from qcgpilotnetsquid.utils.readinput import read_input_json
 from qcgpilotnetsquid.utils.createpoints import create_datapoints
 from qcgpilotnetsquid.qcgpilot.utilsqcgpilot import copyfiles
-from qcgpilotnetsquid.qcgpilot.utilsqcgpilot import commandline
+
+def commandline(j, point, step, general):
+    """Create command line to run jobs
+    Parameters
+    ----------
+        j: int
+            Counter value of datapoints to be explore in optimization step
+        point: array
+            Array with variable values to be explored
+        step: int
+            Optimization step
+        general: dict
+            Information read form json input file
+    Return
+    ------
+        instruction: string
+            Command line instruction to be added to job
+    
+    """
+    instruction = [str(general["program"])]
+    variables = general['parametersarguments'] 
+    if general['configfile'] and general['paramfile']:
+        paramfilename = general['paramfile']
+        paramfilename_new = general['paramfile'] + str(j)
+        configfilename = general['configfile']
+        os.copy(paramfilename, paramfilename_new)
+        for i,v in enumerate(variables):
+#            subprocess.call(["sed", "-i",'s/{}:/poiny[i]/g', paramfilename_new])
+            old = str(v)
+            new = old + ":" + point[i]
+            subprocess.call(['sed -i "s/' + old + '/' + new + '/g"' + paramfilename_new],shell=True)
+        subprocess.call(['sed -i "s/' + parmfilename + '/' + paramfilename_new + '/g"' + configfilename],shell=True)
+        instruction.append("--paramfile " + str(general['configfile']) + "--configfile " + str(general[paramfile]))
+
+    elif general['commandline']:
+        for i, lineargs in enumerate(general['parametersarguments'].split()):
+            instruction.append("--" + str(lineargs))
+            instruction.append(point[i])
+    else:
+        TypeError ("Please specifcy how to run your program")
+
+    if 'outputfile' in general:
+        instruction.append(" --output")
+        instruction.append(str(general['outputfile']))
+    if 'otherarguments' in general:
+        key, value = general["otherarguments"].split()
+        instruction.append("--" + key)
+        instruction.append(value+str(step)+str(j))
+    if 'inputfile' in general:
+        instruction = program + "--" + str(general['inputfile'])
+    return instruction
 
 def directorystructureNLBlueprint(projectdir, general):
     """ Creates the directory structure agreed in the NLbluprint
@@ -191,7 +241,8 @@ def main(inputfile, projectdir):
     algorithm = general['run']['type']['optimization']
     print(algorithm)
 
-    optimization_workflow(sim_param, set_param, infoinput, manager, algorithm, optsteps, optdir, csvfiledir, projectdir)
+    optimization_workflow(sim_param, set_param, infoinput, manager, algorithm,
+optsteps, optdir, csvfiledir, projectdir)
     print("optimization workflow finished")
     
 
